83ba1885ff64a0453abe21f2b80cf1bbdea366da
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-06-03 20:43:17 -0700

    update 2 date

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
 		setSearch(term)
 	}
 
+
 	return (
 		<div>
 			{message}
--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -37,6 +37,7 @@ function AlbumView() {
         )
     }
 
+
     return (
         <div>
             {renderSongs}
